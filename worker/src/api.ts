d10b48a8076d2bd3e35a752aaa7adcfc6b4c4d49
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2023-10-30 17:34:55 -0400

    Update index.ts

--- worker/src/api.ts
+++ worker/src/api.ts
@@ -1,13 +1,50 @@
 
 import { Octokit } from "octokit";
 
-function flattenAndDiscardTopLevel(json) {
-    let result = {};
+const GITHUB_API_VERSION = '2022-11-28';
+const GLOBAL_TRANS_PATH = 'locales/en.json';
+const GLOBAL_KEYS_PATH = 'locales/jp.json';
 
-    for (let key in json) {
-        if (typeof json[key] === 'object' && !Array.isArray(json[key])) {
-            for (let innerKey in json[key]) {
-                result[innerKey] = json[key][innerKey];
+
+function flattenAndDiscardTopLevel(json: { [key: string]: { [innerKey: string]: string } }): { [key: string]: string } {
+    return Object.values(json)
+        .flatMap(value => Object.entries(value))
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+}
+
+
+function decodeBase64(base64String: string): string {
+    const decodedByteString = atob(base64String);
+    const uint8Array = new Uint8Array([...decodedByteString].map(char => char.charCodeAt(0)));
+    return new TextDecoder().decode(uint8Array);
+}
+
+function encodeBase64(input: string): string {
+    return btoa(new TextEncoder().encode(input).reduce((data, byte) => data + String.fromCharCode(byte), ""));
+}
+
+
+
+function containsAllKeys(A: Record<string, any>, B: Record<string, any>): boolean {
+    if (typeof B !== 'object' || B === null || Array.isArray(B)) return true;
+
+    return Object.keys(B).every(key => {
+        if (!(key in A)) return false;
+        return typeof B[key] !== 'object' || containsAllKeys(A[key], B[key]);
+    });
+}
+
+function deepMerge(originalObject: Record<string, any> , newObject: Record<string, any>): Record<string, any> {
+    const result = { ...originalObject };
+
+    for (const key in newObject) {
+        if (newObject.hasOwnProperty(key)) {
+            if (typeof newObject[key] !== 'object' || newObject[key] === null) {
+                result[key] = newObject[key];
+            } else if (typeof originalObject[key] !== 'object' || originalObject[key] === null) {
+                result[key] = newObject[key];
+            } else {
+                result[key] = deepMerge(originalObject[key], newObject[key]);
             }
         }
     }
@@ -15,184 +52,92 @@ function flattenAndDiscardTopLevel(json) {
     return result;
 }
 
-async function updateRepo(pathname: string, request: Request, env: Env) {
 
-    let requestBody = await request.json();
-    pathname = pathname.replace('/en/', '/jp/');
-    // console.log(env.GH_PAGE_URL + pathname);
-    // console.log(requestBody);
-    let jpJsonURLContent = {}
-
-    try { jpJsonURLContent = JSON.parse(await fetch(env.GH_PAGE_URL + pathname).then(response => response.text())); }
-    catch (e) {
-        console.log(e);
-    }
-    // console.log(jpJsonURLContent);
-    if (containsAllKeys(jpJsonURLContent, requestBody)) return;
-    const octokit = new Octokit({ auth: env.REPO_TOKEN });
-    let jpJsonOctokitContent = {}
-    let jpSha: string | undefined;
-
-    try {
-        const result = await getOctokitContent(octokit, env, pathname);
-        jpJsonOctokitContent = result.content;
-        jpSha = result.sha;
-    }
-    catch (e) {
-        console.log(e);
-    }
-    if (jpJsonOctokitContent === undefined) jpJsonOctokitContent = {};
-
-    if (containsAllKeys(jpJsonOctokitContent, requestBody)) return;
-
-    let result = await updateOctokitContent(octokit, env, pathname, jpJsonOctokitContent, requestBody, jpSha);
-    // console.log(result);
-    let allKeysContent = {}
-    let allKeysSha: string | undefined;
-
-    try {
-        const result = await getOctokitContent(octokit, env, 'locales/jp.json');
-        allKeysContent = result.content;
-        allKeysSha = result.sha;
-    }
-    catch (e) {
-        console.log(e);
-    }
-    if (allKeysContent === undefined) allKeysContent = {};
-    // console.log(requestBody);
-    requestBody = flattenAndDiscardTopLevel(requestBody);
-    // console.log(requestBody);
-    if (containsAllKeys(allKeysContent, requestBody)) return;
-
-    result = await updateOctokitContent(octokit, env, 'locales/jp.json', allKeysContent, requestBody, allKeysSha);
-    // console.log(result);
-
-
-}
-function decodeBase64(base64String) {
-    // Decode Base64 string into byte string
-    let decodedByteString = atob(base64String);
-
-    // Convert byte string into a Uint8Array
-    let uint8Array = new Uint8Array(decodedByteString.split("").map(char => char.charCodeAt(0)));
-
-    // Decode Uint8Array into a string using TextDecoder
-    let decoder = new TextDecoder();
-    return decoder.decode(uint8Array);
-}
-
-async function getOctokitContent(octokit: any, env: Env, path: string) {
-    let response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+async function getRepoFile(octokit: any, env: Env, path: string) {
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
         owner: env.REPO_OWNER,
         repo: env.REPO_NAME,
-        path: path,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28',
-            // 'accept': 'application/vnd.github.raw'
-        }
+        path,
+        headers: { 'X-GitHub-Api-Version': GITHUB_API_VERSION }
     });
-    response = response.data;
-    //console.log(response);
-    if (response.encoding === 'base64') {//console.log(decodeBase64(response.content));
-        return {
-            content: JSON.parse(decodeBase64(response.content)),
-
-            sha: response.sha
-        }
-    }
-    return {
-        content: JSON.parse(response.data),
-        sha: response.sha
-    };
+    const content = data.encoding === 'base64' ? JSON.parse(decodeBase64(data.content)) : JSON.parse(data.content);
+    return { content, sha: data.sha };
 }
 
+async function putRepoFile(octokit: any, env: Env, path: string, newKeys: Record<string, any>, sha: string) {
+   
+    const contentBase64 = encodeBase64(JSON.stringify(newKeys, null, 4) + '\n');
 
-function containsAllKeys(A, B) {
-    // If B is not an object or is an array, return true (nothing to check against)
-    if (typeof B !== 'object' || B === null || Array.isArray(B)) {
-        return true;
-    }
-
-    for (let key in B) {
-        // Check if A has the key
-        if (!(key in A)) {
-            return false;
-        }
-
-        // If the value of the current key in B is an object, recursively check its keys
-        if (typeof B[key] === 'object' && B[key] !== null) {
-            if (!containsAllKeys(A[key], B[key])) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-async function updateOctokitContent(octokit: any, env: Env, path: string, content: any, updates: any, sha: string) {
-    for (const ns in updates) {
-        if (typeof updates[ns] !== 'object') {
-            content[ns] = updates[ns];
-            continue;
-        }
-        if (!content[ns]) content[ns] = {};
-        Object.assign(content[ns], updates[ns]);
-    }
-
-    const contentBase64 = toBase64(JSON.stringify(content, null, 4) + '\n');
-    const requestBody = {
+    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
         owner: env.REPO_OWNER,
         repo: env.REPO_NAME,
-        path: path,
+        path,
         message: `Update ${path} with missing keys`,
-        committer: {
-            name: 'Calvin Bot',
-            email: 'dev@kchai.me'
-        },
+        committer: { name: 'Calvin Bot', email: 'dev@kchai.me' },
         content: contentBase64,
-        sha: sha,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
+        sha,
+        headers: { 'X-GitHub-Api-Version': GITHUB_API_VERSION }
+    });
+}
+
+async function fetchAndUpdateRepoContent(octokit: any, env: Env, keyFilePath: string, newKeys: any) {
+    let repoOldKeys, oldFileSHA;
+    try {
+        const result = await getRepoFile(octokit, env, keyFilePath);
+        repoOldKeys = result.content;
+        oldFileSHA = result.sha;
+    } catch (e: any) {
+        console.error(`Failed fetching the repository file: ${e.message}`);
     }
-    
-    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', requestBody);
+
+    repoOldKeys = repoOldKeys || {};
+
+    if (!containsAllKeys(repoOldKeys, newKeys)) {
+        await putRepoFile(octokit, env, keyFilePath, deepMerge(repoOldKeys, newKeys), oldFileSHA);
+    }
+}
+
+async function updateRepo(env: Env, keyFilePath: string, newKeys: any) {
+
+    let webOldKeys = {};
+    try {
+        webOldKeys = JSON.parse(await fetch(new URL(keyFilePath, env.GH_PAGE_URL).toString()).then(res => res.text()));
+    } catch (e: any) {
+        console.error(`Failed fetching old JSON from web: ${e.message}`);
+    }
+
+    if (!containsAllKeys(webOldKeys, newKeys)) {
+        const octokit = new Octokit({ auth: env.REPO_TOKEN });
+        await fetchAndUpdateRepoContent(octokit, env, keyFilePath, newKeys);
+        await fetchAndUpdateRepoContent(octokit, env, GLOBAL_KEYS_PATH, flattenAndDiscardTopLevel(newKeys));
+    }
 }
 
 
-function toBase64(input: string): string {
-    const encoder = new TextEncoder();
-    return btoa(String.fromCharCode(...encoder.encode(input)));
-}
 
 export default {
     async fetch(request: Request, env: Env): Promise<Response> {
-        let pathname = request.headers.get('X-pathname') || '';
-
-        if (pathname.startsWith('/')) {
-            pathname = pathname.slice(1);
-        }
-        if (pathname.endsWith('/')) {
-            pathname = pathname.slice(0, -1);
-        }
-
-        if (!pathname || pathname === '/' || pathname === '') {
-            pathname = 'locales/en.json';
-        } else {
-            pathname = `locales/en/${pathname.replace('/', '_')}.json`;
-        }
-
+        // remove the leading and trailing slash
+        let keyFilePath = (request.headers.get('X-pathname') || '').replace(/^\/|\/$/g, '');
+        
+        keyFilePath = keyFilePath ? `locales/en/${keyFilePath.replace(/\//g, '_')}.json` : GLOBAL_TRANS_PATH;
+        
         if (request.method === 'GET') {
-            const content = await fetch(`${env.GH_PAGE_URL}${pathname}`).then(response => response.text());
-            return new Response(content, { headers: { 'Content-Type': 'application/json' } });
+            const response = await fetch(new URL(keyFilePath, env.GH_PAGE_URL).toString());
+
+            return new Response(response.body, {});
         }
 
-        if (pathname === 'locales/en.json') {
-            return new Response("OK");
-        }
+        if (keyFilePath == GLOBAL_TRANS_PATH) return new Response("Method not supported", { status: 405 });
 
-        await updateRepo(pathname, request, env);
-        return new Response("OK");
+        let newKeys: any;
+        try { newKeys = await request.json(); } catch (e: any) { return new Response(`Failed parsing JSON: ${e.message}`, { status: 400 }); }
+        keyFilePath = keyFilePath.replace('/en/', '/jp/');
+        try {
+            await updateRepo(env, keyFilePath, newKeys);
+            return new Response("OK", { status: 200 });
+        } catch (e: any) {
+            return new Response(`Failed updating repository: ${e.message}`, { status: 500 });
+        }
     }
 };
